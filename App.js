2fd31cd99ee34cb2cb8887897006ed4c939b5f11
ana-111 <anacarolinaamaralsilva2003@gmail.com>
2020-11-18 14:57:20 -0300

    camera finalizada

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Camera from './pages/camera';
+
+const Stack = createStackNavigator();
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Camera" component={Cameras} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
@@ -19,3 +26,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
